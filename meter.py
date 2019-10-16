cf6aa75cc8540ea4864e916fada5081b1b36e327
jason950374 <jasonwanttoplay@gmail.com>
2019-10-16 18:23:05 +0800

    HistMeter implemented with torch.bincount since the in_stream is already constrain to be integer

--- meter.py
+++ meter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import json
 from matplotlib import pyplot as plt
 
@@ -41,7 +42,13 @@ class MaxMinMeter:
 
 
 class HistMeter:
+    r"""
+        If the in_stream is integer,it can be implemented with torch.bincount
+
+    """
     eps = 10 ** -4
+    _init_max = -2 ** 40
+    _init_min = 2 ** 40
 
     def __init__(self, codes=None):
         self.hist = {}
@@ -50,39 +57,46 @@ class HistMeter:
         if codes is not None:
             self.codes = codes
 
-        try:
-            for code in self.codes:
-                self.hist[code] = 0
-        except AttributeError:
-            self.codes = None
+        # gen code length tensor
+
+        self.max = self._init_max
+        self.min = self._init_min
+        self.hist = None
 
     def reset(self, codes=None):
         self.__init__(codes)
 
     def update(self, in_stream):
-        assert (((in_stream % 1) >= (1 - HistMeter.eps)) | ((in_stream % 1) < HistMeter.eps)).min(), \
-            "in_stream need to be integers"
-        cnt = 0
-        '''if len(in_stream.size()) > 1:
+        r"""
+
+        Args:
+            in_stream(torch.Tensor): Input data
+
+        """
+        with torch.no_grad():
+            assert (((in_stream % 1) >= (1 - HistMeter.eps)) | ((in_stream % 1) < HistMeter.eps)).min(), \
+                "in_stream need to be integers"
+            in_stream = (in_stream + HistMeter.eps).int()
             in_stream = in_stream.view(-1)
-        size = in_stream.size(0)'''
-        size = element_cnt(in_stream)
-
-        min_in = in_stream.min()
-        max_in = in_stream.max()
-
-        for code in self.hist:
-            if (code + HistMeter.eps) < min_in:
-                continue
-            elif (code - HistMeter.eps) > max_in:
-                continue
-            match = (in_stream < (code + HistMeter.eps)) & (in_stream >= (code - HistMeter.eps))
-            cnt += int(match.sum().item())
-            self.hist[code] += match.sum().item()
-
-        self.size_total += size
-        assert cnt == size, \
-            "{} vs. {}: Some code in in_stream not find in self.hist".format(cnt, size)
+            min_in = int(in_stream.min())
+            max_in = int(in_stream.max())
+            if self.hist is None:
+                in_stream = in_stream - min_in
+                self.hist = torch.bincount(in_stream)
+                self.max = max_in
+                self.min = min_in
+            else:
+                right_extd = max(max_in - self.max, 0)
+                left_extd = max(self.min - min_in, 0)
+                if right_extd + left_extd > 0:
+                    org_hist = self.hist
+                    self.hist = torch.zeros([(self.max + right_extd + 1) - (self.min - left_extd)])
+                    self.hist = self.hist.to(device=org_hist.device, dtype=org_hist.dtype)
+                    self.hist[left_extd:(left_extd+self.max-self.min+1)] = org_hist
+                    self.max = self.max + right_extd
+                    self.min = self.min - left_extd
+                in_stream = in_stream - self.min
+                self.hist = self.hist + torch.bincount(in_stream, minlength=self.max - self.min + 1)
 
     def get_bit_cnt(self, code_length_dict):
         r"""
@@ -101,55 +115,28 @@ class HistMeter:
             Returns:
                     Total length (bits)
             """
-        total_len = 0
+        with torch.no_grad():
+            weight = []
+            for code in range(self.min, self.max + 1):
+                weight.append(code_length_dict[code])
+            weight = torch.tensor(weight).to(device=self.hist.device, dtype=self.hist.dtype)
 
-        for code in self.hist:
-            total_len += self.hist[code] * code_length_dict[code]
+            total_len = (self.hist * weight).sum()
 
         return total_len
 
     def save_hist_json(self, filename):
         with open(filename, 'w') as f:
-            json.dump(self.hist, f)
+            json.dump({'hist': self.hist.tolist(),
+                       'max': self.max,
+                       'min': self.min,
+                       }, f)
 
-    def plt_hist(self, plt_fn=None, tight=True):
+    def plt_hist(self, plt_fn=None):
         if plt_fn is None:
             plt_fn = plt
 
-        codes = np.asarray(list(self.hist.keys()))
-        codes.sort()
-        xmin = codes[0]
-        xmax = codes[-1]
-
-        hist = []
-        if tight:
-            for code in codes:
-                if self.hist[code] == 0:
-                    xmin = code
-                else:
-                    break
-
-            for code in reversed(codes):
-                if self.hist[code] == 0:
-                    xmax = code
-                else:
-                    break
-
-            assert xmax > xmin, "Nothing??"
-
-            for code in range(xmin, xmax + 1):
-                hist.append(self.hist[code])
-
-            plt_fn.bar(range(xmin, xmax + 1), hist, width=1)
-
-        else:
-            for code in codes:
-                if code != 0:
-                    hist.append(self.hist[code])
-                else:
-                    hist.append(0)
-
-            plt_fn.bar(codes, hist, width=1)
+        plt_fn.bar(range(self.min, self.max + 1), self.hist.cpu(), width=1)
 
 
 def element_cnt(x):
